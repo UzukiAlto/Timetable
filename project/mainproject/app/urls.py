69f481a60d9152fd49499a73f0bc7ae8e7d6b6a6
kete113 <guenminan@gmail.com>
2025-11-12 11:01:46 +0900

    コンフリクト治すのミスったの修正

--- project/mainproject/app/urls.py
+++ project/mainproject/app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'app'
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path("", views.index_is_editing, name="index_is_editing"),
-    path("<str:username>/edit/<int:id_day_of_week>/<int:id_period>/", views.home_edit, name="home_edit"),
+    # path("<str:username>/edit/<int:id_day_of_week>/<int:id_period>/", views.home_edit, name="home_edit"),
+    path("edit/<int:id_day_of_week>/<int:id_period>/", views.home_edit, name="home_edit"),
     path("delete/<int:class_id>/", views.delete_class, name="delete_class"),
 ]
