ac44bc02443274c5fcac55a87b9e2584d37adfe3
alto <uzukialto@gmail.com>
2026-01-11 20:25:36 +0900

    [add] フッター

---
+++ project/mainproject/static/js/global-menu.js
@@ -0,0 +1,8 @@
+const togglerButton = document.querySelector(".navbar-toggler");
+const togglerTarget = document.querySelector(".collapse");
+const currentUrl = window.location.href;
+
+// /accounts/ではヘッダーの要素をフッターに表示するため、ボタンは非表示
+if (currentUrl.includes("/accounts/")){
+    togglerButton.classList.add("hide-content");
+}
